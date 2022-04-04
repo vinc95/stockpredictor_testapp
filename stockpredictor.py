f8201e7a2438332c0ed77b72adbd2d038a0ee895
vinc95 <102709002+vinc95@users.noreply.github.com>
2022-04-04 16:16:36 +0800

    Update stockpredictor.py

--- stockpredictor.py
+++ stockpredictor.py
@@ -34,8 +34,8 @@ st.write(data.tail())
 
 def plot_raw_data():
     fig = plt.Figure()
-    plt.Scatter(x=data['Date'], y=data['Open'], c='b', marker='x', label='stock_open')
-    plt.Scatter(x=data['Date'], y=data['Close'], c='r', marker='s', label='close')
+    plt.scatter(x=data['Date'], y=data['Open'], c='b', marker='x', label='stock_open')
+    plt.scatter(x=data['Date'], y=data['Close'], c='r', marker='s', label='close')
     fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
